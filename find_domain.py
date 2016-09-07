58641dade35f639ff5722a0934cf6742d8b4fb59
Dongdong Zhang <ucabdz0@ucl.ac.uk>
2016-09-07 10:57:38 +0100

    This is for date processing to extract news data from the original
    This is for date processing to extract news data from the original

---
+++ find_domain.py
@@ -0,0 +1,52 @@
+
+import csv
+with open('page_url_and_highlights.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader.next()
+    selections = [x[4] for x in reader]
+    # sentences = ["%s" % (x) for x in sentences]
+f.close()
+#
+with open('page_url_and_highlights.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader.next()
+    urls = [x[5] for x in reader]
+    # sentences = ["%s" % (x) for x in sentences]
+f.close()
+
+with open('page_url_and_highlights.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader.next()
+    domains = [x[7] for x in reader]
+    # sentences = ["%s" % (x) for x in sentences]
+f.close()
+print len(domains)
+j = 0
+valid_domain = ['thedailybeast.com','ft.com','guardian.co.uk','thenewamerican.com','politicususa.com','newyorker.com',\
+                'magazines.scholastic.com','infowars.com','cnn.com','theguardian.com','forbes.com','washingtonpost.com',\
+                'huffingtonpost.com','theatlantic.com','dailymail.co.uk','reuters.com','nytimes.com','bloomberg.com',\
+                'thehindu.com','bbc.co.uk']
+valid_domain2 = 'en.wikipedia.org'
+
+valid_domain3 = ['time.com','abcnews.go.com','investopedia.com','tumblr.com','dailykos.com','wired.com','stlmag.com','edition.cnn.com',\
+                 'rt.com','foxnews.com','sciencedaily.com', 'usatoday.com','reddit.com', 'businessinsider.com', 'latimes.com',\
+                 'telegraph.co.uk', 'psychologytoday.com','bbc.com',]
+
+valid_domain_final = valid_domain + valid_domain3
+
+urls_list = []
+writefile = file('data/17/news_17_whole.csv', 'w')
+writer = csv.writer(writefile)
+for i in range(0,len(domains)):
+    # print domains
+    if domains[i] in valid_domain_final:
+    # if domains[i] == valid_domain2:
+        urls_list.append(urls[i])
+        j += 1
+        writer.writerow((urls[i],selections[i],domains[i]))
+    # if j == 5:
+    #     break
+print j
+print len(urls_list)
+print len(list(set(urls_list)))
+
